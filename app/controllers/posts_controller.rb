39a9e6f76dfa0b525fc9f3a88349bf40cfccde27
RSijelmass <R.Sijelmass@gmail.com>
2017-08-16 09:40:09 +0100

    Add functionality to add comments - comments linked to posts in one-to-many-relationship

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,23 @@
 class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
 	def new
 	end
 
 	def create
-		render plain: params[:post].inspect
+		@post = Post.create(post_params)
+		redirect_to @post
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :text)
 	end
 end
