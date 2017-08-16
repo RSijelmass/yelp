b2db53acbc9aaf9f5856522eb626e32ba59d7221
RSijelmass <R.Sijelmass@gmail.com>
2017-08-16 10:26:45 +0100

    Home page is now index of posts if signed in - after sign in, also let to index of posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 	resources :posts do
 		resources :comments
 	end
+
+	constraints Clearance::Constraints::SignedIn.new do
+    root to: "posts#index", as: :signed_in_root
+  end
 end
